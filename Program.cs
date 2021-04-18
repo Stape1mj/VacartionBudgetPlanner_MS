b70b62b392045b08a3742b10738d010403b79455
Stape1mj <82343937+Stape1mj@users.noreply.github.com>
2021-04-17 20:31:38 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace VacationBudgetPlanner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Thanks for using my application! What is your name?");
+            string name = Console.ReadLine();
+
+           Console.WriteLine($"Hi {name}! Where would you like to travel? Mexico or Italy?");
+
+            Console.ReadLine();
+        }
+    }
+}
